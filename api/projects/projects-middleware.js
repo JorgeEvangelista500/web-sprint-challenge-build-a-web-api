4f28771a3b420c65c1ddb005b8ee751c7d434ac2
Jorge <jorgeevangelista500@gmail.com>
2022-02-11 08:22:18 -0500

    added middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,28 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+module.exports = {
+    validateProjectId,
+    validateProject
+}
+
+async function validateProjectId(req, res, next) {
+    const id =req.params.id;
+    const result = await Projects.get(id)
+    if(result == null) {
+        res.status(404).json({message: 'Project not found'})
+    } else {
+        req.project = result;
+        next();
+    }
+}
+
+function validateProject(req, res, next) {
+    if(!req.body.name) {
+        res.status(400).json({message:'missing required name field'})
+    } else if(!req.body.description) {
+        res.status(400).json({message:'missing required description field'})
+    } else {
+        next()
+    }
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,8 +2,35 @@
 const express = require('express')
 
 const Projects = require('./projects-model')
+const { validateProjectId, validateProject } = require('./projects-middleware')
+const res = require('express/lib/response')
 
 const router = express.Router()
+
+// router.get('/', (req, res) => {
+//     Projects.get(req.)
+//         .then(project => {
+//             res.status(200).json(project)
+//         })
+// })
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.status(200).json(req.project)
+})
+
+router.post('/', validateProject, (req, res) => {
+    Projects.insert(req.body)
+        .then(project => {
+            res.status(200).json(project)
+        })
+})
+
+router.put('/:id', validateProjectId, validateProject, (req, res) => {
+    Projects.update(req.params.id, req.body)
+        .then(newProject => {
+            res.status(200).json(newProject)
+        })
+})
 
 
 module.exports = router;
